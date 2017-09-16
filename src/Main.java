ae4f7e1fa6d5e81d8919f7cafee0a48aaefcf155
Manju-Ch <32010133+Manju-Ch@users.noreply.github.com>
2017-09-16 15:37:55 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+Student stu=new Student(12,manju,13/1/2017,40);
+     stu.getId();
+      stu.setid();
+       
+      stu.getfullName();
+	}
+
+}
